4c14bdf62e240008e83019e7cf4da25d88e68629
bipin321 <beepinpathak0721@gmail.com>
2023-05-12 14:18:47 +0545

    Build

--- src/Pages/Works.jsx
+++ src/Pages/Works.jsx
@@ -5,7 +5,24 @@ import DoctorStoryNepal from '../images/DoctorStoryNepal.png'
 const Works = () => {
   return (
     <div className='sm:w-10/12 m-auto text-center flex flex-col items-center'>
-      <div className='pt-20 sm:pt-10 md:text-5xl text-3xl'><p>Featured Projects</p></div>
+      <div className='pt-20 sm:pt-10 md:text-5xl text-3xl'><p className=''>Featured Projects</p></div>
+      <div className='flex flex-col sm:flex-row sm:flex-wrap sm:justify-between pb-20'>
+        <div className='lg:pt-40 md:pt-20  pt-10'>
+          <img src={TechTemple} alt="" className='project-img rounded-2xl' />
+        </div>
+        <div className='lg:pt-52 md:pt-40 sm:pt-20 pt-10'>
+          <img src={DoctorStoryNepal} alt="" className='project-img rounded-2xl ' />
+
+        </div>
+        <div className='lg:pt-52 md:pt-40 sm:pt-20 pt-10'>
+          <img src={TechTemple} alt="" className='project-img rounded-2xl' />
+        </div>
+        <div className='lg:pt-40 md:pt-20 pt-10'>
+          <img src={DoctorStoryNepal} alt="" className='project-img rounded-2xl ' />
+
+        </div>
+      </div>
+      <div className='pt-20 sm:pt-10 md:text-5xl text-3xl'><p>My Projects</p></div>
       <div className='flex flex-col sm:flex-row sm:flex-wrap sm:justify-between pb-20'>
         <div className='lg:pt-40 md:pt-20  pt-10'>
           <img src={TechTemple} alt="" className='project-img rounded-2xl' />
